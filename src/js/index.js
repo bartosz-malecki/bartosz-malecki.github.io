64d26852636b6f83bece0304ab13bafff3a93a13
Bartosz Malecki <bartoszmalecki90@gmail.com>
2020-11-13 11:35:05 +0100

    feat: API used on projects and some hero styles fixed

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,54 @@ if (media.matches) {
 } else {    
 }
 
+const repositoryList = document.querySelector('.projects-grid');
+
+fetch("https://api.github.com/users/bartosz-malecki/repos")
+.then(resp => resp.json())
+.then(resp => {
+    for (let repo of resp) {
+        const{name, description, homepage, html_url} = repo;
+        const myTemplate = `<article class="project">
+        <div class="project__window">
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+        </div>
+        <div class="project__content">
+          <img
+            class="project__ghLogo"
+            src="../assets/img/ghContent.svg"
+            alt=""
+          />
+          <h3 class="project__title project__grid">
+            <span class="project__label">project:</span>
+            <span>${name}</span>
+          </h3>
+          <p class="project__grid">
+            <span class="project__label">description:</span
+            ><span class="project__description">${description}</span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">demo:</span
+            ><span
+              >&lt;<a href="${homepage}" title="tytuł" class="project__link"
+                >see here</a
+              >&gt;</span
+            >
+          </p>
+          <p class="project__grid">
+            <span class="project__label">github:</span
+            ><span
+              >&lt;<a href="${html_url}" title="link" class="project__link"
+                >source code</a
+              >&gt;</span
+            >
+          </p>
+        </div>
+      </article>`;
+      repositoryList.innerHTML += myTemplate;
+    }    
+})
+.catch(error => {
+    console.log('Nie udało się pobrać')
+})
